6a888bf5f318ae75bde6a9e3ece3aa65690b7213
Leonardo Duarte da Silva <leozeraduarte@gmail.com>
2020-08-11 17:26:06 -0300

    Repo

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.div`
 	--verticalPadding: 24px;
 
 	padding: var(--verticalPadding) var(--horizontalPadding);
+	 overflow: hidden;
 `;
 
 export const Main = styled.div`
@@ -13,16 +14,44 @@ export const Main = styled.div`
 
 	margin: 0 auto;
 	max-width: 1280px;
+
+	@media (min-width: 768px) {
+		flex-direction: row;
+	}
 `;
 
 export const LeftSide = styled.div`
+  	padding: 0 var(--horizontalPadding);
+
 	@media (min-width: 768px) {
 		width: 25%;
 	}
 `;
 
 export const RightSide = styled.div`
+	padding: 0 var(--horizontalPadding);
   	@media (min-width: 768px) {
 		width: 75%;
+	}
+`;
+
+export const Repos = styled.div`
+  	margin-top: var(--verticalPadding);
+
+	> h2 {
+		font-size: 16px;
+		font-weight: normal;
+	}
+
+	> div {
+		margin-top: 8px;
+
+		display:grid;
+		grid-gap: 16px;
+		grid-template-columns: 1fr;
+
+		@media (min-width: 768px) {
+			grid-template-columns: 1fr 1fr;
+		}
 	}
 `;
